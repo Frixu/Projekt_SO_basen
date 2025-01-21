6da26d8259e2660910a9cecdbbfe77292e119659
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-21 16:59:57 +0100

    Update konfiguracja.h

--- konfiguracja.h
+++ konfiguracja.h
@@ -1,24 +1,26 @@
 #ifndef KONFIGURACJA_H
 #define KONFIGURACJA_H
 
+//ograniczenia pojemności basenu
+#define MAX_OLIMPIJSKI 20
+#define MAX_REKREACYJNY 15
+#define MAX_BRODZIK 10
+#define MAX_KLIENCI 45
 
-#define MAX_OLIMPIJSKI 40
-#define MAX_REKREACYJNY 30
-#define MAX_BRODZIK 15
+//klucze 
+#define SEM_KEY 1234
+#define SHM_KEY 5678
 
+//ograniczenia wiekowe na baseny
 #define MIN_WIEK_OLIMPIJSKI 18
 #define MAX_WIEK_BRODZIK 5
 #define MAX_WIEK_PAMPERS 3
 
-#define CZAS_BILETU 5 // w sekundach
+#define CZAS_BILETU 5 // czas biletu w sekundach
 
-#define SEM_KEY 1234
 #define SEM_OLIMPIJSKI 0
 #define SEM_REKREACYJNY 1
 #define SEM_BRODZIK 2
-
-//sciezki do logow
-#define KASJER_LOG "log/kasjer_log.txt"
 
 //makra dla sprawdzenia zasad
 #define CZY_PAMPERS(wiek) ((wiek) <= MAX_WIEK_PAMPERS)
