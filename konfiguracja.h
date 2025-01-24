730e2497b21bb724be962eeb0cf5c0e665f6cb11
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-24 16:28:31 +0100

    Update konfiguracja.h

--- konfiguracja.h
+++ konfiguracja.h
@@ -1,30 +1,35 @@
 #ifndef KONFIGURACJA_H
 #define KONFIGURACJA_H
 
-//ograniczenia pojemności basenu
 #define MAX_OLIMPIJSKI 20
 #define MAX_REKREACYJNY 15
 #define MAX_BRODZIK 10
 #define MAX_KLIENCI 45
 
-//klucze 
 #define SEM_KEY 1234
 #define SHM_KEY 5678
 
-//ograniczenia wiekowe na baseny
 #define MIN_WIEK_OLIMPIJSKI 18
 #define MAX_WIEK_BRODZIK 5
 #define MAX_WIEK_PAMPERS 3
 
-#define CZAS_BILETU 5 // czas biletu w sekundach
-
+#define LICZBA_BASENOW 3
+#define LICZBA_SEMAFOROW 3
+#define CZAS_BILETU 10 // w sekundach
 #define SEM_OLIMPIJSKI 0
 #define SEM_REKREACYJNY 1
 #define SEM_BRODZIK 2
+#define SEM_LOCK 3
+#define MAX_PROBY 5
 
-//makra dla sprawdzenia zasad
-#define CZY_PAMPERS(wiek) ((wiek) <= MAX_WIEK_PAMPERS)
-#define CZY_BRODZIK(wiek) ((wiek) <= MAX_WIEK_BRODZIK)
-#define CZY_OLIMPIJSKI(wiek) ((wiek) >= MIN_WIEK_OLIMPIJSKI)
+#define GREEN   "\033[32m" // Zielony
+#define RED     "\033[31m" // Czerwony
+#define YELLOW "\033[33m" // Zolty
+#define RESET   "\033[0m" // Resetuje kolor
 
-#endif // KONFIGURACJA_H
+typedef struct {
+    int licznik_klientow;
+} LicznikDanych;
+
+#endif
+
