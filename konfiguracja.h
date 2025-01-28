cae6a23aa3905453691682eca9eb358e28ed9aa4
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-28 15:35:22 +0100

    Update konfiguracja.h

--- konfiguracja.h
+++ konfiguracja.h
@@ -1,6 +1,7 @@
 #ifndef KONFIGURACJA_H
 #define KONFIGURACJA_H
 
+
 #define MAX_OLIMPIJSKI 20
 #define MAX_REKREACYJNY 15
 #define MAX_BRODZIK 10
@@ -14,7 +15,7 @@
 #define MAX_WIEK_PAMPERS 3
 
 #define LICZBA_BASENOW 3
-#define LICZBA_SEMAFOROW 3
+#define LICZBA_SEMAFOROW 4
 #define CZAS_BILETU 10 // w sekundach
 #define SEM_OLIMPIJSKI 0
 #define SEM_REKREACYJNY 1
@@ -22,14 +23,19 @@
 #define SEM_LOCK 3
 #define MAX_PROBY 5
 
+#define PROJECT_ID 'L'
+
 #define GREEN   "\033[32m" // Zielony
-#define RED     "\033[31m" // Czerwony
-#define YELLOW "\033[33m" // Zolty
+#define RED     "\033[31m" //Czerwony
+#define YELLOW "\033[33m" //Zolty
 #define RESET   "\033[0m" // Resetuje kolor
 
+
+// Struktura do przechowywania danych o średniej wieku w basenie rekreacyjnym
 typedef struct {
-    int licznik_klientow;
-} LicznikDanych;
+    int suma_wiekow; // Suma wieku wszystkich osób w basenie rekreacyjnym
+    int liczba_osob; // Liczba osób w basenie rekreacyjnym
+} DaneRekreacyjny;
+
 
 #endif
-
