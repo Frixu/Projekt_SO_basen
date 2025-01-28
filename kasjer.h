6da4a7aede40c34fd13561d921930f35b7ca59b8
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-28 15:33:32 +0100

    Create kasjer.h

---
+++ kasjer.h
@@ -0,0 +1,9 @@
+#ifndef KASJER_H
+#define KASJER_H
+
+// Deklaracje funkcji kasjera
+void inicjalizuj_semafory(int sem_id);
+void usun_semafory(int sem_id);
+int czy_wszystkie_baseny_puste(int sem_id);
+
+#endif
