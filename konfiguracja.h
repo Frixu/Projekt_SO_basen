815b227c44c84a8b6c1d55cbb427464b2f72d7eb
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-30 23:50:50 +0100

    Update konfiguracja.h

--- konfiguracja.h
+++ konfiguracja.h
@@ -7,9 +7,6 @@
 #define MAX_BRODZIK 10
 #define MAX_KLIENCI 45
 
-#define SEM_KEY 1234
-#define SHM_KEY 5678
-
 #define MIN_WIEK_OLIMPIJSKI 18
 #define MAX_WIEK_BRODZIK 5
 #define MAX_WIEK_PAMPERS 3
@@ -20,10 +17,7 @@
 #define SEM_OLIMPIJSKI 0
 #define SEM_REKREACYJNY 1
 #define SEM_BRODZIK 2
-#define SEM_LOCK 3
 #define MAX_PROBY 5
-
-#define PROJECT_ID 'L'
 
 #define GREEN   "\033[32m" // Zielony
 #define RED     "\033[31m" //Czerwony
@@ -36,6 +30,5 @@ typedef struct {
     int suma_wiekow; // Suma wieku wszystkich osób w basenie rekreacyjnym
     int liczba_osob; // Liczba osób w basenie rekreacyjnym
 } DaneRekreacyjny;
-
 
 #endif
