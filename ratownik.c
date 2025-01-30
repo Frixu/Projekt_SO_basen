7d974e3f9523064907457e5363cf8ca89fcc4b80
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-30 23:56:06 +0100

    Update ratownik.c

--- ratownik.c
+++ ratownik.c
@@ -1,58 +1,70 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <sys/sem.h>
 #include "ratownik.h"
+
+pid_t ratownicy[3];
 
 void rozpocznij_prace_ratownikow(int sem_id) {
     for (int i = 0; i < LICZBA_BASENOW; i++) {
         pid_t pid = fork();
         if (pid == 0) {
-            setpgid(0, -i - 1);
-            while (1) {
-                pause();
+            setpgid(0, -i -1);
+            while(1){
+            pause();
             }
-            exit(0);
         } else if (pid < 0) {
             perror("Blad przy tworzeniu procesu ratownika");
             exit(1);
         }
+        ratownicy[i] = pid; // Zapisanie pid ratownika
     }
-    printf("Procesy ratowników uruchomione i oczekują na polecenia!\n");
+     printf("Procesy ratowników uruchomione i oczekują na polecenia!\n");
 }
+
+void zakoncz_prace_ratownikow(){
+        printf("Ratownik: Koniec pracy\n");
+        for(int i = 0; i < LICZBA_BASENOW; i++){
+                if(ratownicy[i] > 0){
+                        kill(ratownicy[i], SIGTERM);
+                }
+        }
+}
+
 
 void zamknij_basen(int numer_basen, int sem_id) {
     printf("Ratownik: Zamykam basen %d!\n", numer_basen);
     semctl(sem_id, numer_basen, SETVAL, 0);
-    kill(-numer_basen - 1, SYGNAL_OPUSC_BASEN);
+    killpg(-numer_basen - 1, SYGNAL_OPUSC_BASEN);
 }
 
 void otworz_basen(int numer_basen, int sem_id) {
     printf("Otwieram basen %d!\n", numer_basen);
     semctl(sem_id, numer_basen, SETVAL, 1);
-    kill(-numer_basen - 1, SYGNAL_WROC_NA_BASEN);
+    killpg(-numer_basen - 1, SYGNAL_WROC_NA_BASEN);
 }
 
 void zamknij_cale_centrum(int sem_id) {
-    printf("Zamykam całe centrum pływackie!\n");
-    for (int i = 0; i < LICZBA_BASENOW; i++) {
+    printf("⚠️ Zamykam całe centrum pływackie!\n");
+    for(int i = 0; i < LICZBA_BASENOW; i++){
         semctl(sem_id, i, SETVAL, 0);
     }
-    kill(0, SYGNAL_ZAMKNIJ_OBIEKT);
+    killpg(getpgrp(), SYGNAL_ZAMKNIJ_OBIEKT);
 }
 
 void otworz_cale_centrum(int sem_id) {
     printf("Całe centrum pływackie ponownie otwarte!\n");
-    for (int i = 0; i < LICZBA_BASENOW; i++) {
-        semctl(sem_id, i, SETVAL, 1);
+    for(int i = 0; i < LICZBA_BASENOW; i++){
+            semctl(sem_id, i, SETVAL, 1);
     }
-    kill(0, SYGNAL_WROC_NA_BASEN);
+    killpg(getpgrp(), SYGNAL_WROC_NA_BASEN);
 }
 
 void menu_ratownika(int sem_id) {
     int wybor;
-    while (1) {
+    do{
         printf("\n===== MENU RATOWNIKA =====\n");
         printf("1. Zamknij basen Olimpijski\n");
         printf("2. Zamknij basen Rekreacyjny\n");
@@ -67,47 +79,48 @@ void menu_ratownika(int sem_id) {
         scanf("%d", &wybor);
 
         switch (wybor) {
-            case 1: 
-                zamknij_basen(0, sem_id); 
-                break;
-            case 2: 
-                zamknij_basen(1, sem_id);
-                break;
-            case 3: 
-                zamknij_basen(2, sem_id); 
-                break;
-            case 4: 
-                otworz_basen(0, sem_id); 
-                break;
-            case 5: 
-                otworz_basen(1, sem_id); 
-                break;
-            case 6: 
-                otworz_basen(2, sem_id); 
-                break;
-            case 7: 
-                zamknij_cale_centrum(sem_id); 
-                break;
-            case 8: 
-                otworz_cale_centrum(sem_id); 
-                break;
-            case 9: 
-                exit(0);
-            default: 
-                printf("Niepoprawny wybór!\n");
+            case 1:
+                    zamknij_basen(0, sem_id);
+                    break;
+            case 2:
+                    zamknij_basen(1, sem_id);
+                    break;
+            case 3:
+                    zamknij_basen(2, sem_id);
+                    break;
+            case 4:
+                    otworz_basen(0, sem_id);
+                    break;
+            case 5:
+                    otworz_basen(1, sem_id);
+                    break;
+            case 6:
+                    otworz_basen(2, sem_id);
+                    break;
+            case 7:
+                    zamknij_cale_centrum(sem_id);
+                    break;
+            case 8:
+                    otworz_cale_centrum(sem_id);
+                    break;
+            case 9:
+                    zakoncz_prace_ratownikow();
+                    exit(0);
+            default:
+                    printf("❌ Niepoprawny wybór!\n");
         }
-    }
+    }while(wybor != 9);
 }
 
 int main() {
     key_t sem_key = ftok("konfiguracja.h", 'A');
     int sem_id = semget(sem_key, LICZBA_BASENOW, 0666);
     if (sem_id == -1) {
-        perror("Błąd przy pobieraniu semaforów - czy kasjer je stworzył?");
+        perror("Blad przy pobieraniu semaforó");
         exit(1);
     }
 
     rozpocznij_prace_ratownikow(sem_id);
     menu_ratownika(sem_id);
-    return 0;
+        return 0;
 }
