21bbbcc8a8ebd2e269c2a79ef5860f3ba3533f97
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-30 23:52:29 +0100

    Update klient.h

--- klient.h
+++ klient.h
@@ -2,17 +2,22 @@
 #define KLIENT_H
 
 #include "konfiguracja.h"
+#include "ratownik.h"
 
-// Struktura do przechowywania danych o średniej wieku w basenie rekreacyjnym
-/*typedef struct {
-    int suma_wiekow; // Suma wieku wszystkich osób w basenie rekreacyjnym
-    int liczba_osob; // Liczba osób w basenie rekreacyjnym
-} DaneRekreacyjny;*/
-
-// Funkcje
+// Funkcja do obliczania sredniej na basenie rekreacyjnym
 double oblicz_srednia(DaneRekreacyjny *dane);
-void ewakuacja(int sig);
-void inicjalizuj_sygnaly();
+
+// Funkcja symulujaca wchodzenie na basen
 void wejdz_na_basen(int wiek, int sem_id, int sem_num, int is_vip, int numer, DaneRekreacyjny *rekreacyjny, int is_opiekun);
 
+// Funkcja wywoływana, gdy klient otrzyma sygnał do opuszczenia basenu
+void opusc_basen(int sig);
+
+// Funkcja wywoływana, gdy klient otrzyma sygnał do powrotu na basen
+void wroc_na_basen(int sig);
+
+//Funkcja wywoływana gdy będzie wysłany sygnał zamkniecia calego centrum
+void zamkniecie_obiektu(int sig);
+
 #endif
+
