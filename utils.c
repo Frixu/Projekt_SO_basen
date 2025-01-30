1ba1674deb8d7d5f82b42695bc9b52c8b3646df6
Frixu <157427965+Frixu@users.noreply.github.com>
2025-01-30 23:57:52 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -3,7 +3,15 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
+#include <signal.h>
 #include "utils.h"
+#include "kasjer.h"
+
+int sem_id, shm_id, rek_id; // Globalne ID zasobów
+int *klient_numer;
+DaneRekreacyjny *rekreacyjny;
+
+volatile sig_atomic_t sigint_received = 0;
 
 // Funkcja do uzyskiwania klucza pamięci współdzielonej
 key_t get_shm_key(const char *pathname, int project_id) {
@@ -15,20 +23,20 @@ key_t get_shm_key(const char *pathname, int project_id) {
     return shm_key;
 }
 
-// Funkcja do uzyskiwania klucza pamięci współdzielonej
-key_t get_sem_key(const char *pathname, int project_id) {
-    key_t sem_key = ftok(pathname, project_id);
-    if (sem_key == -1) {
-        perror("Blad przy generowaniu klucza pamieci wspoldzielonej");
-        exit(1);
-    }
-    return sem_key;
+key_t get_sem_key(const char *pathname, int project_id){
+        key_t sem_key = ftok(pathname, project_id);
+        if(sem_key == -1){
+                perror("Blad przy generowaniu klucza semaforow");
+                exit(1);
+        }
+        return sem_key;
 }
+
 // Funkcja do tworzenia lub uzyskiwania semafora
 int create_semaphore(int sem_key, int num_semaphores) {
     int sem_id = semget(sem_key, num_semaphores, 0666 | IPC_CREAT);
     if (sem_id == -1) {
-        perror("Blad przy tworzeniu semaforów");
+        perror("Blad przy tworzeniu semaforó");
         exit(1);
     }
     return sem_id;
@@ -70,3 +78,16 @@ void usun_pamiec_wspoldzielona(int *klient_numer, DaneRekreacyjny *rekreacyjny, 
     printf("Pamiec wspoldzielona zostala usunieta.\n");
 }
 
+void handler_sigint(int sig) {
+    if(sigint_received){
+            return;
+    }
+    sigint_received = 1;
+
+    printf("\nOtrzymano sygnal CTRL+C\n");
+    usun_pamiec_wspoldzielona(klient_numer, rekreacyjny, shm_id, rek_id);
+    usun_semafory(sem_id);
+
+    printf("Zasoby usunięte. Kończę działanie.\n");
+    exit(0);
+}
